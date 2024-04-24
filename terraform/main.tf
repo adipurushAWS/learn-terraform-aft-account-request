a375cd0ce55a2999fc377eefca57f3a2e8a1a765
adipurushAWS <167025007+adipurushAWS@users.noreply.github.com>
2024-04-24 19:58:51 -0400

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,30 +1,3 @@
-
-
-
-module "sandboxdev-aft" {
-  source = "./modules/aft-account-request"
-
-  control_tower_parameters = {
-    AccountEmail              = "adipurush.aws+sandboxdev@gmail.com"
-    AccountName               = "sandboxdev-aft"
-    ManagedOrganizationalUnit = "Sandbox"
-    SSOUserEmail              = "adipurush.aws+sandboxdev@gmail.com"
-    SSOUserFirstName          = "SandboxDev"
-    SSOUserLastName           = "AFT"
-  }
-
-  account_tags = {
-    "Learn Tutorial" = "AFT"
-  }
-
-  change_management_parameters = {
-    change_requested_by = "sandboxdev-aft"
-    change_reason       = "Agent based AFT account creation"
-  }
-  account_customizations_name = "sandbox"
-}
-
-
 module "Dev-NonProd" {
   source = "./modules/aft-account-request"
 
