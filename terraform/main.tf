c5452f1e1fafbccfa8a8fadecf616e598ad06845
adipurushAWS <167025007+adipurushAWS@users.noreply.github.com>
2024-04-24 19:44:28 -0400

    Append new Terraform module for Dev-NonProd

--- terraform/main.tf
+++ terraform/main.tf
@@ -23,3 +23,27 @@ module "sandboxdev-aft" {
   }
   account_customizations_name = "sandbox"
 }
+
+
+module "Dev-NonProd" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "adikelkar.aws+dev@gmail.com"
+    AccountName               = "Dev-NonProd"
+    ManagedOrganizationalUnit = "Non-Prod"
+    SSOUserEmail              = "adikelkar.aws+dev@gmail.com"
+    SSOUserFirstName          = "Dev"
+    SSOUserLastName           = "NP"
+  }
+
+  account_tags = {
+    "Learn Tutorial" = "AFT"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Dev-NonProd"
+    change_reason       = "Adding dev account"
+  }
+  account_customizations_name = "san"
+}
