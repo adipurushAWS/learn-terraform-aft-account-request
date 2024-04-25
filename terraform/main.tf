e508b6d09a1534edc7f48a4a3845790217c95dde
adipurushAWS <167025007+adipurushAWS@users.noreply.github.com>
2024-04-25 16:43:35 -0400

    Append new Terraform module for Test-NonProd

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,3 +20,28 @@ module "Dev-NonProd" {
   }
   account_customizations_name = "san"
 }
+
+
+module "Test-NonProd" {
+  source = "./modules/aft-account-request"
+
+  control_tower_parameters = {
+    AccountEmail              = "adikelkar.aws+test@gmail.com"
+    AccountName               = "Test-NonProd"
+    ManagedOrganizationalUnit = "Non-Prod"
+    SSOUserEmail              = "adikelkar.aws+test@gmail.com"
+    SSOUserFirstName          = "Test"
+    SSOUserLastName           = "NP"
+  }
+
+  account_tags = {
+    "type" = "AFT",
+    "source" = "bedrock-agent"
+  }
+
+  change_management_parameters = {
+    change_requested_by = "Test-NonProd"
+    change_reason       = "Adding test account"
+  }
+  account_customizations_name = "sandbox"
+}
